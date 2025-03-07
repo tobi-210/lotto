f107ed7aec648c8a047a7e3513a88a545145d187
tobi-210 <tobiasknapp2010@gmail.com>
2025-03-07 08:10:40 +0000

    sortingprogram

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-  int main(int argc, char **argv)
+int main(int argc, char **argv)
+{
+
+  int32_t array[10];
+  int32_t biggestnumber;
+
+  printf("\n");
+
+  printf("WELCOME TO THE SORTING PROGRAM!\n");
+
+  printf("\n");
+
+  for (int i = 0; i < 10; i++)
   {
+    printf("Number %d is: ", i + 1);
+    scanf("%d", &array[i]);
+  }
 
-    int32_t array[10];
-    int32_t biggestnumber;
-
-    printf("\n");
-
-    printf("WELCOME TO THE SORTING PROGRAM!\n");
-
-    printf("\n");
-
-    for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = 0; j < 9; j++)
     {
-      printf("Number %d is: ", i + 1);
-      scanf("%d", &array[i]);
-    }
-
-    for (int i = 0; i < 9; i++)
-    {
-      for (int j = 0; j < 9; j++)
+      if (array[j] < array[j + 1])
       {
-        if (array[j] < array[j + 1])
-        {
-          biggestnumber = array[j];
-          array[j] = array[j + 1];
-          array[j + 1] = biggestnumber;
-        }
+        biggestnumber = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = biggestnumber;
       }
     }
+  }
   printf("Die sortierten Zahlen sind: ");
-  for(int i = 0; i < 10; i++)
+  for (int i = 0; i < 10; i++)
   {
     printf("%d ", array[i]);
   }
